a3971e4802eb4b88687a155bc6415ce23ed5fe30
Abere Fejiro <aberefejiro@gmail.com>
2023-12-05 18:30:39 +0100

    interactivity using css added

--- script/script.js
+++ script/script.js
@@ -2,6 +2,14 @@ let toggleMenu = document.querySelector('#toggle-menu');
 let menu = document.querySelector('#menu');
 
 toggleMenu.addEventListener('click', () => {
-    console.log('clicked')
-    menu.classList.toggle('open-menu')
-});
+    // toggleMenuIcon();
+    menu.classList.toggle('open-menu');
+})
+
+function toggleMenuIcon() {
+    let hamburger = './images/icon-hamburger.svg';
+    let close_hamburger = './images/icon-close.svg';
+
+    toggleMenu.src = (toggleMenu.src === hamburger) ? close_hamburger : hamburger;
+    console.log(toggleMenu.src);
+}
