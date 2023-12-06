00fccdb252b9e7793c3e326fa1b6a07cd6d297d1
Abere Fejiro <aberefejiro@gmail.com>
2023-12-06 23:14:41 +0100

    submission time

--- script/script.js
+++ script/script.js
@@ -2,14 +2,16 @@ let toggleMenu = document.querySelector('#toggle-menu');
 let menu = document.querySelector('#menu');
 
 toggleMenu.addEventListener('click', () => {
-    // toggleMenuIcon();
+    toggleMenuIcon();
     menu.classList.toggle('open-menu');
 })
 
+let isMenuOpen = false;
 function toggleMenuIcon() {
     let hamburger = './images/icon-hamburger.svg';
     let close_hamburger = './images/icon-close.svg';
 
-    toggleMenu.src = (toggleMenu.src === hamburger) ? close_hamburger : hamburger;
+    toggleMenu.src = isMenuOpen ? hamburger : close_hamburger;
+    isMenuOpen = !isMenuOpen;
     console.log(toggleMenu.src);
 }
